3a7358a4680f74e68b89f559f19ce09c4b524735
Max Hoogenbosch <max@bluemammoth.nl>
2019-08-13 12:45:54 +0200

    Added uri-editor

---
+++ uri-editor/precache-manifest.b94558d2b6231d49cd4cb838521d9905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fedad9dcf10d2e13fdaf164501ce6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c15e20eab0e389352fb2",
+    "url": "/static/css/main.4e19be3d.chunk.css"
+  },
+  {
+    "revision": "fe76cea1694fc00d52d4",
+    "url": "/static/js/2.db62d070.chunk.js"
+  },
+  {
+    "revision": "c15e20eab0e389352fb2",
+    "url": "/static/js/main.9b03a02a.chunk.js"
+  },
+  {
+    "revision": "bf4d9f715a8c125d5657",
+    "url": "/static/js/runtime~main.c6b1c1cb.js"
+  }
+]);
